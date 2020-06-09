1e77de3ef132ed98a7d1a6333c2f2799a293264d
evyooo <yooooojining@gmail.com>
2020-06-09 21:18:08 +0900

    Create datechange in calendar

--- Android/FamApp/app/src/main/java/com/example/famapp/Calendar/CalendarAdapter.kt
+++ Android/FamApp/app/src/main/java/com/example/famapp/Calendar/CalendarAdapter.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -21,29 +22,40 @@ class CalendarAdapter(var context: Context, var arrayList: ArrayList<String>): B
         var circle = view.findViewById<ImageView>(R.id.selected_circle)
         var each = view.findViewById<ConstraintLayout>(R.id.each_conslay)
 
+        var box1 = view.findViewById<Button>(R.id.box1_button_cal)
+        var box2 = view.findViewById<Button>(R.id.box2_button_cal)
+        var box3 = view.findViewById<Button>(R.id.box3_button_cal)
+
         circle.visibility = View.INVISIBLE
+
+        if (arrayList[position] == ""){
+            box1.visibility = View.INVISIBLE
+            box2.visibility = View.INVISIBLE
+            box3.visibility = View.INVISIBLE
+
+        }
+        else{
+            each.setOnClickListener {
+                //  todo
+//            circle.visibility = View.VISIBLE
+
+                val dialog = AlertDialog.Builder(context)
+                val edialog : LayoutInflater = LayoutInflater.from(context)
+                val mView : View = edialog.inflate(R.layout.layout_dialog_calendar,null)
+
+                val date : TextView = mView.findViewById(R.id.textView50)
+
+                date.text = position.toString()
+
+                dialog.setView(mView)
+                dialog.create()
+                dialog.show()
+
+            }
+        }
 
 
         date.text = arrayList[position]
-
-
-        each.setOnClickListener {
-            //  todo
-//            circle.visibility = View.VISIBLE
-
-            val dialog = AlertDialog.Builder(context)
-            val edialog : LayoutInflater = LayoutInflater.from(context)
-            val mView : View = edialog.inflate(R.layout.layout_dialog_calendar,null)
-
-            val date : TextView = mView.findViewById(R.id.textView50)
-
-            date.text = position.toString()
-
-            dialog.setView(mView)
-            dialog.create()
-            dialog.show()
-
-        }
 
 
         return view
--- Android/FamApp/app/src/main/java/com/example/famapp/Calendar/CalendarFragment.kt
+++ Android/FamApp/app/src/main/java/com/example/famapp/Calendar/CalendarFragment.kt
@@ -1,6 +1,7 @@
 package com.example.famapp.Calendar
 
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -9,16 +10,19 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.famapp.R
 import kotlinx.android.synthetic.main.fragment_calendar.*
-import android.widget.TextView
 import android.content.Context.LAYOUT_INFLATER_SERVICE
 import androidx.core.content.ContextCompat.getSystemService
-import android.widget.BaseAdapter
 import android.content.Context
+import android.util.DisplayMetrics
 import android.util.Log
-import android.widget.Toast
+import android.view.WindowManager
+import android.widget.*
+import androidx.core.util.rangeTo
 import androidx.recyclerview.widget.GridLayoutManager
+import kotlinx.android.synthetic.main.fragment_stats.*
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
+import java.time.Month
 import java.time.format.DateTimeFormatter
 import java.util.*
 
@@ -54,24 +58,106 @@ class CalendarFragment : Fragment() {
         //  TODO
 
 
+        dialog_imageview_calendar.setOnClickListener {
 
-        drawCalendar()
+            val dialog = AlertDialog.Builder(context).create()
+            val edialog : LayoutInflater = LayoutInflater.from(context)
+            val mView : View = edialog.inflate(R.layout.dialog_datepicker,null)
+
+            val year : NumberPicker = mView.findViewById(R.id.yearpicker_datepicker)
+            val month : NumberPicker = mView.findViewById(R.id.monthpicker_datepicker)
+
+            //  순환 안되게 막기
+            year.wrapSelectorWheel = false
+
+            //  editText 설정 해제
+            year.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+            month.wrapSelectorWheel = false
+            month.descendantFocusability = NumberPicker.FOCUS_BLOCK_DESCENDANTS
+
+
+            val cancel : Button = mView.findViewById(R.id.cancel_button_datepicker)
+            val save : Button = mView.findViewById(R.id.save_button_datepicker)
+
+
+            //  TODO ..  최대 최소 설정
+            year.minValue = 2019
+            year.maxValue = 2020
+
+            year.setDisplayedValues(arrayOf("2019년", "2020년"))
+
+
+            month.minValue = 1
+            month.maxValue = 12
+
+            month.setDisplayedValues(arrayOf("1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"))
+
+
+            dialog.setView(mView)
+            dialog.create()
+            dialog.show()
+
+
+//            var window = dialog.window
+//            window.setLayout(238 * , WindowManager.LayoutParams.WRAP_CONTENT)
+
+
+            //  취소
+            cancel.setOnClickListener {
+                dialog.dismiss()
+                dialog.cancel()
+            }
+
+            //  완료
+            save.setOnClickListener {
+
+                year_textview_calendar.text = (year.value).toString() + "년"
+                month_textview_calendar.text = (month.value).toString() + "월"
+
+
+                //  TODO ..  서버에 값 넘겨주고 불러오기
+
+                drawCalendar((year.value).toString(), (month.value).toString())
+                Log.d("", "${year.value} ${month.value}")
+
+
+                dialog.dismiss()
+                dialog.cancel()
+            }
+        }
+
+
+        //  날짜
+        val current = LocalDateTime.now()
+        drawCalendar(current.year.toString(), current.monthValue.toString())
+        Log.d("1", "${current.year}")
+
+        year_textview_calendar.text = current.year.toString() + "년"
+        month_textview_calendar.text = current.monthValue.toString() + "월"
 
 
 
-        calendarAdapter = CalendarAdapter(requireContext(), calendarlist)
-        calendar_grid.adapter = calendarAdapter
+
 
 
 
     }
 
-    fun drawCalendar(){
+    fun drawCalendar(year: String, month: String){
 
         calendarlist.clear()
 
+        Log.d("", "$year $month")
 
-        var date = "20200601"
+        var mon = month
+
+        if (mon.length == 1){
+            mon = "0" + mon
+        }
+
+
+        var date = "${year}${mon}01"
         var dateType = "yyyyMMdd"
 
         val dateFormat = SimpleDateFormat(dateType)
@@ -94,13 +180,12 @@ class CalendarFragment : Fragment() {
 
 
 
-        //  날짜
-        val current = LocalDateTime.now()
+
         var totalday = 0
 
 
 
-        when (current.dayOfMonth){
+        when (month.toInt()){
             1, 3, 5, 7, 8, 10, 12 -> totalday = 31
             4, 6, 9, 11 -> totalday = 30
             2 -> totalday = 28
@@ -112,9 +197,7 @@ class CalendarFragment : Fragment() {
 
 
 
-
-
-        var date1 = "20200630"
+        var date1 = "${year}${mon}${totalday}"
 
         val nDate1 = dateFormat.parse(date1)
 
@@ -130,6 +213,11 @@ class CalendarFragment : Fragment() {
                 calendarlist.add("")
             }
         }
+
+
+
+        calendarAdapter = CalendarAdapter(requireContext(), calendarlist)
+        calendar_grid.adapter = calendarAdapter
 
 
 
