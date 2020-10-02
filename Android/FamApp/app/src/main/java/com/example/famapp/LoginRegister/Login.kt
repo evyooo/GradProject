07bae20f507e7ab65502f902355b788c8b7b24f3
evyooo <yooooojining@gmail.com>
2020-10-03 03:36:15 +0900

    Update 네이버로 로그인

--- Android/FamApp/app/src/main/java/com/example/famapp/LoginRegister/Login.kt
+++ Android/FamApp/app/src/main/java/com/example/famapp/LoginRegister/Login.kt
@@ -14,12 +14,18 @@ import com.example.famapp.Global.Companion.basic_url
 import com.example.famapp.MainActivity
 import com.example.famapp.MyPreference
 import com.example.famapp.R
+import com.nhn.android.naverlogin.OAuthLogin
+import com.nhn.android.naverlogin.OAuthLogin.mOAuthLoginHandler
+import com.nhn.android.naverlogin.OAuthLoginHandler
 import kotlinx.android.synthetic.main.activity_login.*
 import org.json.JSONObject
 
 class Login : AppCompatActivity() {
 
     lateinit var myPreference: MyPreference
+
+    lateinit var mOAuthLoginInstance : OAuthLogin
+    lateinit var mContext: Context
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -61,6 +67,38 @@ class Login : AppCompatActivity() {
             login(userid, userpw)
         }
 
+        //  네아로
+        val naver_client_id = "pDfDW6xEY5apvquqnnhT"
+        val naver_client_secret = "ynQOjNXTZq"
+        val naver_client_name = "룸메이트"
+
+        mContext = this
+
+        mOAuthLoginInstance = OAuthLogin.getInstance()
+        mOAuthLoginInstance.init(mContext, naver_client_id, naver_client_secret, naver_client_name)
+
+        buttonOAuthLoginImg.setOAuthLoginHandler(mOAuthLoginHandler)
+
+    }
+
+    val mOAuthLoginHandler: OAuthLoginHandler = object : OAuthLoginHandler() {
+        override fun run(success: Boolean) {
+            if (success) {
+//                val accessToken: String = mOAuthLoginModule.getAccessToken(baseContext)
+//                val refreshToken: String = mOAuthLoginModule.getRefreshToken(baseContext)
+//                val expiresAt: Long = mOAuthLoginModule.getExpiresAt(baseContext)
+//                val tokenType: String = mOAuthLoginModule.getTokenType(baseContext)
+//                var intent = Intent(this, )
+            } else {
+                val errorCode: String = mOAuthLoginInstance.getLastErrorCode(mContext).code
+                val errorDesc = mOAuthLoginInstance.getLastErrorDesc(mContext)
+
+                Toast.makeText(
+                    baseContext, "errorCode:" + errorCode
+                            + ", errorDesc:" + errorDesc, Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
     }
 
     fun bring_index(userid: String){
